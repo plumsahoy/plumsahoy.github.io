2be77b059ead98b40a0e9cee9229e47c63a524b8
plumsahoy <plumdnxu@gmail.com>
2024-07-12 14:54:56 -0400

    Add files via upload

---
+++ spin.js
@@ -0,0 +1,45 @@
+var topics = [];
+
+// assigns elements from HTML for reuse
+const s_button = document.getElementById ("spin_button");
+const result = document.getElementById("wheel_result");
+
+async function getText(file) {
+    let topic_file = await fetch(file);
+    let topic_text = await topic_file.text();
+    console.log(topic_text);
+    return topic_text;
+}
+
+function encodeArray(longstring) {
+    let new_array = longstring.split((/(\r\n|\n|\r)/gm));
+    console.log(new_array);
+    return new_array;
+}
+
+function encodeTopicEntries() {
+
+}
+
+async function defaultTopic() {
+    let topic_text = await getText("topics.txt");
+    topics = encodeArray(topic_text);
+    readyWheel();
+}
+
+function fileTopic() {
+    readyWheel();
+}
+
+function manualTopic() {
+    readyWheel();
+}
+
+function readyWheel() {
+    s_button.disabled = false;
+}
+
+function spin() {
+    const random = Math.floor(Math.random() * topics.length);
+    result.innerHTML = "Result: " + topics[random];
+}
